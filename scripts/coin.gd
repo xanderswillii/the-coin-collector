e439d4ad65f563d19fc62ff008f4cd881e08dd1f
xanderswillii <chedjouwilliam465@gmail.com>
2025-09-28 18:53:38 +0100

    Add files via upload

---
+++ scripts/coin.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+@onready var game_manager: Node = %"game manager"
+
+func _on_body_entered(body: Node2D) -> void:
+	game_manager.add_point()
+	queue_free()
---
+++ scripts/game_manager.gd
@@ -0,0 +1,11 @@
+extends Node
+
+var score = 0
+
+@onready var score_label: Label = $"score label"
+
+func add_point():
+	score += 1
+	score_label.text = "you collected" + str(score) + "coins." 
+	
+	
